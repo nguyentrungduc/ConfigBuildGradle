7e72292a537bd650f328d09cfe606593bd0b568a
Admin <nguyenduc049@gmail.com>
2019-03-02 21:01:16 +0700

    add new module

--- app/src/main/java/com/example/ntduc/buildgradleconfig/MainActivity.kt
+++ app/src/main/java/com/example/ntduc/buildgradleconfig/MainActivity.kt
@@ -6,28 +6,9 @@ import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
-    var filepath = ArrayList<String>()//contains list of all files ending with
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun walkdir(dir: File) {
-        val listFile = dir.listFiles()
-
-        if (listFile != null) {
-            for (i in listFile.indices) {
-
-                if (listFile[i].isDirectory) {// if its a directory need to get the files under that directory
-                    walkdir(listFile[i])
-                } else {// add path of  files to your arraylist for later use
-
-                    //Do what ever u want
-                    filepath.add(listFile[i].absolutePath)
-
-                }
-            }
-        }
-    }
 }
